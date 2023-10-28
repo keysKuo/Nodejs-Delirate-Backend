1a6095d89f4ba003f36626bcf70448b1a70cea61
keyskuo <nkeyskuo124@gmail.com>
2023-10-28 21:01:20 +0700

    Change db to Atlas

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+const DB_URL = process.env.ATLAS_URL || 'mongodb://127.0.0.1:27017/Delirate';
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Delirate', {
+        await mongoose.connect(DB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
