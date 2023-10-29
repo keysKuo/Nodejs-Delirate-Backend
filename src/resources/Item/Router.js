622e4fc3c6fad3a88bdbae278048a9cd258ef511
keyskuo <nkeyskuo124@gmail.com>
2023-10-29 15:55:31 +0700

    Fix create-item ui

--- src/resources/Item/Router.js
+++ src/resources/Item/Router.js
@@ -1,6 +1,10 @@
 import { GET_AllItems, GET_ItemInfo, POST_CreateItem } from "./Resolver.js";
 import express from 'express';
 import upload from '../../middlewares/multer.js';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const base_url = `D:/Study/Blockchain/NEAR-MERN-Delirate-Blockchain2023/`;
 const router = express.Router();
 
@@ -9,6 +13,6 @@ router.get('/get_all_items', GET_AllItems);
 
 router.post('/create_item', upload.single('item-image'),  POST_CreateItem);
 router.get('/create_item', (req, res, next) => {
-    return res.sendFile('create.html');
+    return res.sendFile(path.join(__dirname,'create.html'));
 })
 export default router
