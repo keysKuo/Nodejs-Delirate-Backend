9c072e25e3a8b9f3488ed634867923a64ecd7eb5
keyskuo <nkeyskuo124@gmail.com>
2023-11-13 06:42:11 +0700

    fix rejected permission

--- src/resources/Account/Resolver.js
+++ src/resources/Account/Resolver.js
@@ -366,7 +366,6 @@ async function GET_AnswerPermissionLoginQR(req, res, next) {
         })
     }
     else {
-        delete tokens[token];
         return res.json({
             success: false,
             status: 400,
