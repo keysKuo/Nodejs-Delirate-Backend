1a0641eec9859c48b26582016cc1add81182a9fc
keyskuo <nkeyskuo124@gmail.com>
2023-11-13 05:17:06 +0700

    fix format time localestring

--- src/resources/Account/Resolver.js
+++ src/resources/Account/Resolver.js
@@ -284,7 +284,7 @@ async function GET_LoginQR(req, res, next) {
     const browser = req.useragent.browser;
     const os = req.useragent.os;
 
-    tokens[token].agent = { browser, os, timestamp: Date.now().toLocaleString('vi-vn'), location: 'Ho Chi Minh City' };
+    tokens[token].agent = { browser, os, timestamp: new Date().toLocaleString(), location: 'Ho Chi Minh City' };
 
     const loginUrl = apiUrl + `/account/login_qr?token=${token}`;
 
