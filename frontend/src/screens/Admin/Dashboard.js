fc84e7c0948eb2147a9c939e89fb25bb334ed931
keyskuo <nkeyskuo124@gmail.com>
2023-11-12 15:35:25 +0700

    ignore frontend

--- frontend/src/screens/Admin/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Container } from "semantic-ui-react";
-
-export default function DashboardScreen() {
-    return (
-        <>    
-            <h1 style={{ paddingTop: '60px'}}>Dashboard Screen</h1>
-        </>
-    );
-}
